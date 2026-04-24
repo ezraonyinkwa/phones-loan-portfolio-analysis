eb4f3278c06509bd7fb6722a534536e7a66a5cb0
Ezra Onyinkwa <139281995+ezraonyinkwa@users.noreply.github.com>
2026-04-24 16:03:28 +0300

    Enhance comments for data preparation steps
    Added comments for clarity and understanding of the code.

--- data_preparation.py
+++ data_preparation.py
@@ -3,17 +3,20 @@
 customer_dob['date_of_birth'] = pd.to_datetime(customer_dob['date_of_birth'], errors='coerce') #--Change the date format to datetime and handle errors by coercing them to NaT
 #Quality check after format change-If it worked
 customer_dob['date_of_birth'].dtype
-#Extract only the date part from the datetime column
+#Extract only the date part from the datetime column.
 customer_dob['date_of_birth'] = customer_dob['date_of_birth'].dt.date
 
-#Calculate age from date of birth
+#Calculate age from date of birth.
 today = dt.date.today()
 customer_dob['age'] = customer_dob['date_of_birth'].apply(lambda x: (today - x).days // 365 if pd.notnull(x) else None)
 
-#Group ages into age groups
+#Group ages into age groups.
 bins = [0, 18, 25, 35, 45, 55, 65, float('inf')]
 labels = ['0-18', '19-25', '26-35', '36-45', '46-55', '56-65', '65+']
 customer_dob['age_group'] = pd.cut(customer_dob['age'], bins=bins, labels=labels, right=False)  
 
-#Quality check after age group creation
+#Quality check after age group creation.
 customer_dob['age_group'].value_counts()
+
+#--------------------Average Monthly Income---------------------
+#Hard to use since no Income section in the credit dataset and the customer income dataset is not linked to the credit dataset by any common identifier.
